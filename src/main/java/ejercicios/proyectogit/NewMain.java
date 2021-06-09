333853ca0a0c2154a23cfe758cbc70957051af2e
Maria González Alonso <mariaglezalonso@gmail.com>
2021-06-09 03:02:13 +0200

    Primera entrega dev

--- src/main/java/ejercicios/proyectogit/NewMain.java
+++ src/main/java/ejercicios/proyectogit/NewMain.java
@@ -11,5 +11,5 @@ package ejercicios.proyectogit;
  */
 public class NewMain {
  public static void main(String[] args) {
- System.out.println(Sumador.sumar (15f,4f));
+ System.out.println(Restador.restar(15f,4f));
 }}
